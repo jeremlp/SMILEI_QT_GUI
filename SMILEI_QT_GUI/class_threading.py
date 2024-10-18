a4186d7738a0d192ba79765cdb92110ac3ebb221
Jeremy La Porte <53497875+jeremlp@users.noreply.github.com>
2024-10-18 18:06:17 +0200

    V0.10.10 - Alias IPython, extent & t_range plasma, encryption, reset scalars

--- SMILEI_QT_GUI/class_threading.py
+++ SMILEI_QT_GUI/class_threading.py
@@ -29,7 +29,7 @@ class ThreadDownloadSimJSON(QtCore.QThread):
         print("THREAD downloadSimJSON")
         host = "llrlsi-gw.in2p3.fr"
         user = "jeremy"
-        with open('tornado_pwdfile.txt', 'r') as f: pwd_crypt = f.read()
+        with open('../tornado_pwdfile.txt', 'r') as f: pwd_crypt = f.read()
         pwd = encrypt(pwd_crypt,-2041000*2-1)
         remote_path = r"\sps3\jeremy\LULI\simulations_info.json"
         ssh_key_filepath = r"C:\Users\jerem\.ssh\id_rsa.pub"
